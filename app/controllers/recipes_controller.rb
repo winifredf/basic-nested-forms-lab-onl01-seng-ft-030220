46985f1066db34f543bd791fd6b327ef8a970761
winifredf <winifred.dykes@hotmail.com>
2020-06-15 01:32:39 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,10 @@
 class RecipesController < ApplicationController
+  
   def show
     @recipe = Recipe.find(params[:id])
+    if @recipe.ingredients.last.try(:name)
+      @recipe.ingredients.build
+    end
   end
 
   def index
@@ -9,8 +13,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
